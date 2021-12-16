59f8d60694fe1e6775b4056a549499ce79cd3756
rxdps93 <dpsirott@gmail.com>
2021-12-15 22:42:25 -0500

    puzzle 1 progress

--- day11/puzzle1/d11p1.c
+++ day11/puzzle1/d11p1.c
@@ -3,33 +3,62 @@
 #include "../../common/utils.h"
 
 #define SIZE	10
-#define STEPS	100
+#define STEPS	10
+#define ADJ		8
 
 typedef struct Octopus Octopus;
 
 struct Octopus {
 	int power_level;
 
-	Octopus *adjacent[8];
+	Octopus *adjacent[ADJ];
 };
 
 void print_octopuses(Octopus octopuses[SIZE][SIZE]) {
 	for (int row = 0; row < SIZE; row++) {
 		for (int col = 0; col < SIZE; col++) {
-			printf("%d", octopuses[row][col].power_level);
+			printf("%2d ", octopuses[row][col].power_level);
 		}
 		printf("\n");
 	}
 }
 
-void octopus_flash(Octopus octopuses[SIZE][SIZE]) {
+void octopus_flash(Octopus *octopus, int *flashes) {
 
+	octopus->power_level = 0;
+	*flashes += 1;
+	for (int i = 0; i < ADJ; i++) {
+		if (octopus->adjacent[i]) {
+			if (octopus->adjacent[i]->power_level != 0) {
+				octopus->adjacent[i]->power_level++;
+			}
+
+			if (octopus->adjacent[i]->power_level == 10) {
+				octopus_flash(octopus->adjacent[i], flashes);
+			}
+		}
+	}
 }
 
-void increase_power_level(Octopus octopuses[SIZE][SIZE]) {
+void increase_power_level(Octopus octopuses[SIZE][SIZE], int *flashes) {
 	for (int row = 0; row < SIZE; row++) {
 		for (int col = 0; col < SIZE; col++) {
-			octopuses[row][col].power_level++;
+
+			if (octopuses[row][col].power_level != 0) {
+				octopuses[row][col].power_level++;
+			}
+
+			if (octopuses[row][col].power_level == 10) {
+				octopus_flash(&octopuses[row][col], flashes);
+			}
+		}
+	}
+
+	for (int row = 0; row < SIZE; row++) {
+		for (int col = 0; col < SIZE; col++) {
+			if (octopuses[row][col].power_level > 9) {
+				octopuses[row][col].power_level = 0;
+			}
 		}
 	}
 }
@@ -45,18 +74,53 @@ void map_octopuses(Octopus octopuses[SIZE][SIZE]) {
 			}
 
 			// Up Middle
+			if (row != 0) {
+				octopuses[row][col].adjacent[1] = &octopuses[row - 1][col];
+			} else {
+				octopuses[row][col].adjacent[1] = NULL;
+			}
 
 			// Up Right
+			if (row != 0 && col < (SIZE - 1)) {
+				octopuses[row][col].adjacent[2] = &octopuses[row - 1][col + 1];
+			} else {
+				octopuses[row][col].adjacent[2] = NULL;
+			}
 
 			// Left
+			if (col != 0) {
+				octopuses[row][col].adjacent[3] = &octopuses[row][col - 1];
+			} else {
+				octopuses[row][col].adjacent[3] = NULL;
+			}
 
 			// Right
+			if (col < (SIZE - 1)) {
+				octopuses[row][col].adjacent[4] = &octopuses[row][col + 1];
+			} else {
+				octopuses[row][col].adjacent[4] = NULL;
+			}
 
 			// Down Left
+			if (row < (SIZE - 1) && col != 0) {
+				octopuses[row][col].adjacent[5] = &octopuses[row + 1][col - 1];
+			} else {
+				octopuses[row][col].adjacent[5] = NULL;
+			}
 
 			// Down Middle
+			if (row < (SIZE - 1)) {
+				octopuses[row][col].adjacent[6] = &octopuses[row + 1][col];
+			} else {
+				octopuses[row][col].adjacent[6] = NULL;
+			}
 
 			// Down Right
+			if (row < (SIZE - 1) && col < (SIZE - 1)) {
+				octopuses[row][col].adjacent[7] = &octopuses[row + 1][col + 1];
+			} else {
+				octopuses[row][col].adjacent[7] = NULL;
+			}
 		}
 	}
 }
@@ -86,12 +150,21 @@ int main() {
 		}
 	}
 
+	printf("Before any steps:\n");
+	print_octopuses(octopuses);
+	printf("\n");
+
 	map_octopuses(octopuses);
+
+	int flashes = 0;
 	for (int i = 0; i < STEPS; i++) {
-		increase_power_level(octopuses);
-		octopus_flash(octopuses);
+		printf("After step %d:\n", i + 1);
+		increase_power_level(octopuses, &flashes);
+		print_octopuses(octopuses);
+		printf("\n");
 	}
 
+	printf("After step %d there have been a total of %d flashes.\n", STEPS, flashes);
 	fclose(input);
 	return EXIT_SUCCESS;
 }
