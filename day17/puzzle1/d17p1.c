f84a23d79549604a9e2b40a09217772fbfa35198
rxdps93 <dpsirott@gmail.com>
2022-01-03 09:42:06 -0500

    day 17 puzle 1 complete

--- day17/puzzle1/d17p1.c
+++ day17/puzzle1/d17p1.c
@@ -4,36 +4,42 @@
 #include <math.h>
 #include <limits.h>
 
+int get_y_velocity(int y_min, int y_max) {
+    
+    int max_h = INT_MIN;
+    int max_n = INT_MIN;
+    for (int i = y_min; i < y_max; i++) {
+        double n = abs(i) - 1;
+        double h = (n * (n + 1)) / 2;
+        if ((int)h > max_h) {
+            max_h = (int)h;
+            max_n = (int)n;
+        }
+    }
+    return max_n;
+}
+
 int get_x_velocity(int x_min, int x_max) {
-    // x = ((-b +- sqrt(b^2 - 4ac)) / 2a)
     int x_vel = 0;
     for (int c = x_min; c < x_max + 1; c++) {
-        // printf("trying %d...\n", c);
+
         int dscrmnt = 1 - (4 * (2 * -c));
-        // printf("discriminant is %d...\n", dscrmnt);
         if (dscrmnt >= 0) {
             double root = sqrt(dscrmnt);
 
             if (floor(root) == root) {
                 double pos = (-1 + root) / 2;
                 double neg = (-1 - root) / 2;
-
-                printf("pos=%f; neg=%f\n", pos, neg);
                 
                 if (pos > 0 && neg > 0) {
-                    printf("opt a\n");
                     x_vel = pos < neg ? pos : neg;
                     break;
                 } else if (pos > 0 && neg <= 0) {
-                    printf("opt b\n");
                     x_vel = pos;
                     break;
                 } else if (neg > 0 && pos <= 0) {
-                    printf("opt c\n");
                     x_vel = neg;
                     break;
-                } else {
-                    printf("wtf\n");
                 }
             }
         }
@@ -46,7 +52,7 @@ int main() {
 
     FILE *input;
 
-    if ((input = fopen("../test_input.txt", "r")) == NULL) {
+    if ((input = fopen("../input.txt", "r")) == NULL) {
         printf("Unable to open file");
         exit(EXIT_FAILURE);
     }
@@ -54,36 +60,22 @@ int main() {
     int tx_min, tx_max, ty_min, ty_max;
     fscanf(input, "target area: x=%d..%d, y=%d..%d", &tx_min, &tx_max, &ty_min, &ty_max);
 
-    printf("Target Area: x:(%d -> %d); y:(%d -> %d)\n", tx_min, tx_max, ty_min, ty_max);
+    // printf("Target Area: x:(%d -> %d); y:(%d -> %d)\n", tx_min, tx_max, ty_min, ty_max);
 
-    // FOR X: (n * (n + 1)) / 2 >= tx_min but <= tx_max; try for each value starting with tx_min
-    // (n * (n + 1)) / 2 = 21
-    // (n * (n + 1)) = 42
-    // n^2 + n = 42
-    // n^2 + n - 42 = 0
-    // ax^2 + bx + c = 0 (basic quadratic formula)
-    // x = -b +- sqrt(b^2 - 4ac) / 2a
-    // x = -1 +- sqrt(1 - (4 * 1 * -42)) / 2
-    // x = -1 +- sqrt(1 + 168) / 2
-    // x = -1 +- 13 / 2
-    // x = (-1 + 13 / 2), (-1 - 13 / 2)
-    // x = (12 / 2), (-14 / 2)
-    // x = 6, -7
-    // x = 6
     const int x_init = get_x_velocity(tx_min, tx_max);
-    const int y_init = 9;
+    const int y_init = get_y_velocity(ty_min, ty_max);
     int x_velocity = x_init;
     int y_velocity = y_init;
     int x = 0;
     int y = 0;
     int max_height = INT_MIN;
-    int step = 0;
+    // int step = 0;
     bool running = true;
-    printf("Using initial velocity (x=%d, y=%d)\n", x_velocity, y_velocity);
+    // printf("Using initial velocity (x=%d, y=%d)\n", x_velocity, y_velocity);
     while (running) {
         x += x_velocity;
         y += y_velocity;
-        printf("After step %d: x=%d, y=%d...", step++, x, y);
+        // printf("After step %d: x=%d, y=%d...", step++, x, y);
 
         if (y > max_height) {
             max_height = y;
@@ -97,19 +89,19 @@ int main() {
         y_velocity--;
 
         if (x > tx_max && x_velocity >= 0) {
-            printf("Too far right; won't make it\n");
+            // printf("Too far right; won't make it\n");
             running = false;
         } else if (x < tx_min && x_velocity <= 0) {
-            printf("Too far left; won't make it\n");
+            // printf("Too far left; won't make it\n");
             running = false;
         } else if (y < ty_min) {
-            printf("Too far down; overshot it\n");
+            // printf("Too far down; overshot it\n");
             running = false;
         } else if ((x >= tx_min && x <= tx_max) && (y >= ty_min && y <= ty_max)) {
-            printf("IN THE TARGET\n");
+            // printf("IN THE TARGET\n");
             running = false;
         } else {
-            printf("\n");
+            // printf("\n");
         }
     }
 
