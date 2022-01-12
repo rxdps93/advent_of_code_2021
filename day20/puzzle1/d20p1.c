0d3d4583ee1c836c86ef0182015f3f47e867a804
rxdps93 <dpsirott@gmail.com>
2022-01-11 22:46:13 -0500

    padding

--- day20/puzzle1/d20p1.c
+++ day20/puzzle1/d20p1.c
@@ -1,17 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define TEST 1
-#define STEPS 2
+#define STEPS 1
 #define ALGORITHM_SIZE 512
 #define INPUT_SIZE (TEST ? 5 : 100)
+
+void print_img(const int *img, const int size) {
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
+            printf("%d", img[row * size + col]);
+        }
+        printf("\n");
+    }
+}
 
 int map_char(char c) {
     return c == '.' ? 0 : c == '#' ? 1 : -1;
 }
 
-char map_int(int i) {
-    return i ? '#' : '.';
+int pad_input(int *input_img, int old_size, int filler) {
+
+    int new_size = old_size + 4;
+    int new_img[new_size][new_size];
+    
+    for (int i = 0; i < new_size; i++) {
+        new_img[0][i] = filler;
+        new_img[1][i] = filler;
+        new_img[new_size - 2][i] = filler;
+        new_img[new_size - 1][i] = filler;
+
+        new_img[i][0] = filler;
+        new_img[i][1] = filler;
+        new_img[i][new_size - 2] = filler;
+        new_img[i][new_size - 1] = filler;
+    }
+
+    for (int row = 0; row < old_size; row++) {
+        for (int col = 0; col < old_size; col++) {
+            new_img[row + 2][col + 2] = input_img[row * old_size + col];
+        }
+    }
+
+    input_img = realloc(input_img, new_size * new_size * sizeof(int));
+    memcpy(input_img, new_img, sizeof new_img);
+
+    return new_size;
 }
 
 int main() {
@@ -41,18 +76,20 @@ int main() {
     }
     fclose(input);
 
-    for (int r = 0; r < INPUT_SIZE; r++) {
-        for (int c = 0; c < INPUT_SIZE; c++) {
-            printf("%d", input_img[r * INPUT_SIZE + c]);
-        }
-        printf("\n");
-    }
-
     // Iterate for X steps:
     // Pad array with some (at least 2?) layers of the appropriate value (0 or 1, changes each time)
     // Output = Input
     // Start writing changes to output
+    // Trim output down to an appropriate size
     // Save output as new input
+    int size = INPUT_SIZE;
+    int filler = 0;
+    for (int i = 0; i < STEPS; i++) {
+        size = pad_input(input_img, size, filler);
+        filler = 1 - filler;
+        print_img(input_img, size);
+    }
 
+    free(input_img);
     return EXIT_SUCCESS;
 }
