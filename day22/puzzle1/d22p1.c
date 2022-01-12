93b08c6092a49d9894d0a64fa4d28da3deee0e02
rxdps93 <dpsirott@gmail.com>
2022-01-12 17:01:22 -0500

    day 22 start

---
+++ day22/puzzle1/d22p1.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+
+#define TEST 1
+
+typedef struct Vector {
+    int x;
+    int y;
+    int z;
+} vector_t;
+
+typedef struct Cube {
+    int state;
+    vector_t pos;
+
+    int change[500];
+    int inst_cnt;
+} cube_t;
+
+typedef struct Instruction {
+    int state;
+
+    int x_min;
+    int x_max;
+    
+    int y_min;
+    int y_max;
+
+    int z_min;
+    int z_max;
+} ins_t;
+
+int read_input(ins_t ins_set[500], FILE *input) {
+    int ins_cnt = -1;
+    char c;
+    do {
+        fgetc(input);
+        c = fgetc(input);
+        ins_cnt++;
+        if (c == 'n') {
+            ins_set[ins_cnt].state = 1;
+            fgetc(input);
+        } else {
+            ins_set[ins_cnt].state = 0;
+            fseek(input, 2, SEEK_CUR);
+        }
+
+    } while(fscanf(input, "x=%d..%d,y=%d..%d,z=%d..%d\n",
+            &ins_set[ins_cnt].x_min, &ins_set[ins_cnt].x_max,
+            &ins_set[ins_cnt].y_min, &ins_set[ins_cnt].y_max,
+            &ins_set[ins_cnt].z_min, &ins_set[ins_cnt].z_max) == 6);
+
+    return ins_cnt;
+}
+
+int main() {
+    FILE *input;
+    char *file_name = TEST == 1 ? "../test_input_small.txt" : TEST == 2 ? "../test_input_large.txt" : "../input.txt";
+
+    if ((input = fopen(file_name, "r")) == NULL) {
+        printf("Unable to open file");
+        exit(EXIT_FAILURE);
+    }
+
+    ins_t ins_set[500];
+    int ins_cnt = read_input(ins_set, input);
+    fclose(input);
+
+    vector_t min = { INT_MAX, INT_MAX, INT_MAX };
+    vector_t max = { INT_MIN, INT_MIN, INT_MIN };
+    for (int i = 0; i < ins_cnt; i++) {
+
+        // x min
+        if (ins_set[i].x_min < min.x) {
+            min.x = ins_set[i].x_min;
+        }
+
+        // x max
+        if (ins_set[i].x_max > max.x) {
+            max.x = ins_set[i].x_max;
+        }
+
+        // y min
+        if (ins_set[i].y_min < min.y) {
+            min.y = ins_set[i].y_min;
+        }
+
+        // y max
+        if (ins_set[i].y_max > max.y) {
+            max.y = ins_set[i].y_max;
+        }
+
+        // z min
+        if (ins_set[i].z_min < min.z) {
+            min.z = ins_set[i].z_min;
+        }
+
+        // z max
+        if (ins_set[i].z_max > max.z) {
+            max.z = ins_set[i].z_max;
+        }
+    }
+    int num_cubes = (abs(max.x - min.x) + 1) * (abs(max.y - min.y) + 1) * (abs(max.y - min.y) + 1);
+
+    cube_t cubes[num_cubes];
+    printf("There are a total of %d cubes\n", num_cubes);
+
+    return EXIT_SUCCESS;
+}
