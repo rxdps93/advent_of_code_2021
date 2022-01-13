a95eef7a51d9313d43c294386889e7502fb51307
rxdps93 <dpsirott@gmail.com>
2022-01-12 22:41:58 -0500

    d22p1 work

--- day22/puzzle1/d22p1.c
+++ day22/puzzle1/d22p1.c
@@ -16,7 +16,7 @@ typedef struct Cube {
     vector_t pos;
 
     int change[500];
-    int inst_cnt;
+    int ins_cnt;
 } cube_t;
 
 typedef struct Instruction {
@@ -31,6 +31,20 @@ typedef struct Instruction {
     int z_min;
     int z_max;
 } ins_t;
+
+int vectors_match(vector_t a, vector_t b) {
+    return a.x == b.x && a.y == b.y && a.z == b.z;
+}
+
+int find_cube(cube_t *cubes, int size, vector_t pos) {
+    for (int i = 0; i < size; i++) {
+        if (vectors_match(cubes[i].pos, pos) && cubes[i].ins_cnt != -1) {
+            return i;
+        }
+    }
+
+    return -1;
+}
 
 int read_input(ins_t ins_set[500], FILE *input) {
     int ins_cnt = -1;
@@ -102,10 +116,33 @@ int main() {
             max.z = ins_set[i].z_max;
         }
     }
-    int num_cubes = (abs(max.x - min.x) + 1) * (abs(max.y - min.y) + 1) * (abs(max.y - min.y) + 1);
+    int num_cubes = (abs(max.x - min.x) + 1) * (abs(max.y - min.y) + 1) * (abs(max.z - min.z) + 1);
 
     cube_t cubes[num_cubes];
-    printf("There are a total of %d cubes\n", num_cubes);
+    for (int i = 0; i < num_cubes; i++) {
+        cubes[i].ins_cnt = -1;
+    }
+
+    int c;
+    vector_t pos;
+    for (int i = 0; i < ins_cnt; i++) {
+        for (int x = ins_set[i].x_min; x <= ins_set[i].x_max; x++) {
+            for (int y = ins_set[i].y_min; y <= ins_set[i].y_max; y++) {
+                for (int z = ins_set[i].z_min; z <= ins_set[i].z_max; z++) {
+                    pos.x = x;
+                    pos.y = y;
+                    pos.z = z;
+                    int c = find_cube(cubes, num_cubes, pos);
+
+                    if (c == -1) {
+
+                    }
+
+                    
+                }
+            }
+        }
+    }
 
     return EXIT_SUCCESS;
 }
