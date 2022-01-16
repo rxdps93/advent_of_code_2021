a8c705dd0cec040cac2963b4753d7e77a16d1c4b
rxdps93 <dpsirott@gmail.com>
2022-01-16 13:56:11 -0500

    more work

--- day23/puzzle1/d23p1.c
+++ day23/puzzle1/d23p1.c
@@ -4,9 +4,49 @@
 
 #define TEST 1
 
-void parse_input(FILE *input) {
+int is_room_finished(room_t room) {
+    if (room.top.occupant != NULL && room.bottom.occupant != NULL) {
+        return room.type == room.top.occupant->type == room.bottom.occupant->type;
+    }
+
+    return 0;
+}
+
+int is_organized(burrow_t *state) {
+    // Check the hallway
+    for (int i = 0; i < 11; i++) {
+        if (state->hallway.spots[i].occupant != NULL) {
+            return 0;
+        }
+    }
+
+    // Check the rooms
+    for (int i = 0; i < 4; i++) {
+        if (is_room_finished(state->rooms[i])) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+void organize_burrow(burrow_t state) {
+
+    int visit_num = 0;
+    int visit_cap = 1024;
+    burrow_t *visited = (burrow_t *)malloc(visit_cap * sizeof(burrow_t));
+
+
+}
+
+burrow_t parse_input(FILE *input) {
 
     burrow_t state;
+    state.energy = 0;
+    state.rooms[0].type = AMBER;
+    state.rooms[1].type = BRONZE;
+    state.rooms[2].type = COPPER;
+    state.rooms[3].type = DESERT;
 
     char c;
     char str[50];
@@ -14,39 +54,69 @@ void parse_input(FILE *input) {
     while (fscanf(input, "%[^A,B,C,D]s", str) == 1) {
         c = fgetc(input);
 
-        amphipod_t amphi;
         switch (c) {
             case 'A':
-                amphi.type = AMBER;
+                state.amphis[count].type = AMBER;
                 break;
             case 'B':
-                amphi.type = BRONZE;
+                state.amphis[count].type = BRONZE;
                 break;
             case 'C':
-                amphi.type = COPPER;
+                state.amphis[count].type = COPPER;
                 break;
             case 'D':
-                amphi.type = DESERT;
+                state.amphis[count].type = DESERT;
                 break;
         }
 
         if (c == 'A' || c == 'B' || c == 'C' || c == 'D') {
-
-        }
-
-        printf("%c", c);
-        count++;
-
-        if (count == 4 || count == 8) {
-            printf("\n");
-
-            if (count == 8) {
-                break;
+            // 0->-4; 1->-3; 2->-2, 3->-1 == top row
+            if (count - 4 < 0) {
+                state.amphis[count].moves = 2;
+                state.rooms[count].top.occupant = &state.amphis[count];
+                state.amphis[count].space = &state.rooms[count].top;
+                state.rooms[count].top.down = &state.rooms[count].bottom;
+            } else {
+                // 4->1; 5->2; 6->3; 7->4 == bottom row
+                state.amphis[count].moves = (state.amphis[count].type == state.rooms[count - 4].type) ? 0 : 2;
+                state.rooms[count - 4].bottom.occupant = &state.amphis[count];
+                state.amphis[count].space = &state.rooms[count - 4].bottom;
+                state.rooms[count - 4].bottom.up = &state.rooms[count-4].top;
             }
+
+            count++;
         }
     }
 
-    // return state;
+    return state;
+}
+
+void map_burrow(burrow_t *burrow) {
+    
+    for (int i = 0; i < 11; i++) {
+
+        burrow->hallway.spots[i].up = NULL;
+        burrow->hallway.spots[i].occupant = NULL;
+
+        if (i == 0) {
+            burrow->hallway.spots[i].left = NULL;
+        } else {
+            burrow->hallway.spots[i].left = &burrow->hallway.spots[i - 1];
+        }
+
+        if (i == 10) {
+            burrow->hallway.spots->right = NULL;
+        } else {
+            burrow->hallway.spots[i].right = &burrow->hallway.spots[i + 1];
+        }
+
+        if (i == 2 || i == 4 || i == 6 || i == 8) {
+            burrow->hallway.spots[i].down = &burrow->rooms[(i / 2) - 1].top;
+            burrow->rooms[(i / 2) - 1].top.up = &burrow->hallway.spots[i];
+        } else {
+            burrow->hallway.spots[i].down = NULL;
+        }
+    }
 }
 
 int main() {
@@ -58,8 +128,12 @@ int main() {
         printf("Unable to open file");
         exit(EXIT_FAILURE);
     }
-    parse_input(input);
-    // burrow_t state = parse_input(input);
+
+    burrow_t state = parse_input(input);
+    map_burrow(&state);
+
+    print_rooms(&state);
+    print_burrow(&state);
 
     fclose(input);
     return EXIT_SUCCESS;
--- day23/utils/d23_structs.h
+++ day23/utils/d23_structs.h
@@ -32,10 +32,9 @@ struct Hallway {
 
 struct Room {
     amphitype_t type;
-    space_t *hall_left;
-    space_t *hall_right;
 
-    space_t spots[2];
+    space_t top;
+    space_t bottom;
 };
 
 struct Burrow {
@@ -44,3 +43,76 @@ struct Burrow {
     hall_t hallway;
     room_t rooms[4];
 };
+
+char type_char(amphitype_t type) {
+    switch (type) {
+        case AMBER:
+            return 'A';
+        case BRONZE:
+            return 'B';
+        case COPPER:
+            return 'C';
+        case DESERT:
+            return 'D';
+    }
+
+    return '?';
+}
+
+void print_rooms(burrow_t *burrow) {
+    for (int i = 0; i < 4; i++) {
+        printf("room %c top: %c, moves=%d\n",
+            type_char(burrow->rooms[i].type),
+            type_char(burrow->rooms[i].top.occupant->type),
+            burrow->rooms[i].top.occupant->moves);
+        printf("room %c bottom: %c, moves=%d\n",
+            type_char(burrow->rooms[i].type),
+            type_char(burrow->rooms[i].bottom.occupant->type),
+            burrow->rooms[i].bottom.occupant->moves);
+    }
+}
+
+void print_burrow(burrow_t *burrow) {
+    char out[5][14];
+    for (int i = 1; i < 12; i++) {
+        out[0][i] = '#';
+        out[4][i] = ' ';
+        if (burrow->hallway.spots[i - 1].occupant == NULL) {
+            out[1][i] = '.';
+        } else {
+            out[1][i] = type_char(burrow->hallway.spots[i - 1].occupant->type);
+        }
+
+        if (burrow->hallway.spots[i - 1].down == NULL) {
+            out[2][i] = '#';
+            out[3][i] = ' ';
+        } else {
+            if (burrow->hallway.spots[i - 1].down->occupant == NULL) {
+                out[2][i] = '.';
+            } else {
+                out[2][i] = type_char(burrow->hallway.spots[i - 1].down->occupant->type);
+            }
+
+            if (burrow->hallway.spots[i - 1].down->down->occupant == NULL) {
+                out[3][i] = '.';
+            } else {
+                out[3][i] = type_char(burrow->hallway.spots[i - 1].down->down->occupant->type);
+            }
+            out[3][i - 1] = '#';
+            out[4][i - 1] = '#';
+            out[4][i] = '#';
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        out[i][13] = '\0';
+        if (i < 3) {
+            out[i][0] = '#';
+            out[i][12] = '#';
+        } else {
+            out[i][0] = ' ';
+            out[i][12] = ' ';
+        }
+        printf("%s\n", out[i]);
+    }
+}
